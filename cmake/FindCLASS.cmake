49843c5cbd2b265bfbbe7257023346c529cbe5ed
Mouginot B <mouginot.baptiste@gmail.com>
2018-07-27 22:37:54 -0500

    fix

--- cmake/FindCLASS.cmake
+++ cmake/FindCLASS.cmake
@@ -60,7 +60,7 @@ SET(CLASS_ROOT_DIR "${CLASS_INCLUDE_DIR}/../..")
 
 # Look for the library
 FIND_LIBRARY(CLASS_LIBRARY 
-    NAMES CLASSpkg
+    NAMES libCLASSpkg.so
     HINTS "${CLASS_ROOT_DIR}" "${CLASS_ROOT_DIR}/lib" "$ENV{CLASS_PATH}/lib"
     "$ENV{CLASS_lib}"
     /usr/local/CLASS/lib /usr/local/CLASS
